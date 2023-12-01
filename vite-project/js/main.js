fe3f64b17c20fb7c3c5502837901b3c1fc33c2d1
kennethn23 <kennethng1337@gmail.com>
2023-12-01 09:24:18 -0500

    12/1/23

---
+++ vite-project/js/main.js
@@ -0,0 +1,15 @@
+import '../css/style.css'
+
+const url = `https://valorant-api.com/v1/agents`;
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  } catch (error) {
+
+  }
+}
+
+getData(url);
