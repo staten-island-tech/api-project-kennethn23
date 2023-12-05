b47cedf0bb4f63b616cde6af058ea632685f8a79
kennethn23 <kennethng1337@gmail.com>
2023-12-05 09:23:18 -0500

    12/5/23

--- vite-project/js/inserts.js
+++ vite-project/js/inserts.js
@@ -1,18 +1,52 @@
 import { DOMSelectors } from './dom';
 
-async function insertCard () {
-    // get random agent
-    // get random desc, ability icon, ability desc
-}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-async function insertTitle () {
-    DOMSelectors.box.insertAdjacentHTML("beforeend", 
-    `<div class="card">
-      <h1 id="title">Guess the VALORANT Agent!</h1>
-      <h3 id="description">You will be given a random agent's description, ability icon, or ability description.</h3>
-    </div>
-    <button type="submit" class="playButton">PLAY</button>
-    `)
-}
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    insertCard(data);
+  } catch (error) {
 
-export { insertCard, insertTitle };
+  }
+};
+
+async function insertCard (array) {
+  console.log(array);
+  /*let questionType = 0;
+  // let questionType = getRandomInt(0, 2);
+  const url = `https://valorant-api.com/v1/agents`;
+
+  if (questionType == 0) {
+    // agent description
+    // let agent = getRandomInt(0, getData(url).length)
+    let data = getData(url);
+    console.log(getData(url));
+
+  } else if (questionType == 1) {
+    // ability icon
+    console.log("a")
+
+  } else if (questionType == 2) {
+    // ability description
+    console.log("b")
+
+  }*/
+};
+
+function insertTitle () {
+  DOMSelectors.box.insertAdjacentHTML("beforeend", 
+  `<div class="title">
+    <h1 id="title">Guess the VALORANT Agent!</h1>
+    <h3 id="description">You will be given a random agent's description, ability icon, or ability description.</h3>
+  </div>
+  <button type="submit" class="playButton">PLAY</button>
+  `)
+};
+
+export { insertCard, insertTitle, getData };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,11 @@
 import '../css/style.css'
 import { DOMSelectors } from './dom';
-import { insertCard, insertTitle } from './inserts';
+import { insertCard, insertTitle, getData } from './inserts';
 
-/*const url = `https://valorant-api.com/v1/agents`;
+// const url = `https://valorant-api.com/v1/agents`;
+const url = `https://api.quotable.io/random`;
 
-async function getData(url) {
+/*async function getData(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -14,6 +15,11 @@ async function getData(url) {
   }
 }
 
-getData(url);*/
+// getData(url);*/
 
 insertTitle();
+
+document.querySelector(".playButton").addEventListener("click", function() {
+  DOMSelectors.box.innerHTML = "";
+  getData();
+})
